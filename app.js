d1ad6d183e10cbe0c08ae1f9c845f9902f2699d2
Anjali Divekar <anjalidivekar2002@gmail.com>
2021-06-10 10:24:15 +0530

    add: items to ul and append

---
+++ app.js
@@ -0,0 +1,55 @@
+// SELECTORS
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+// EVENT LISTENERS
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+
+// FUNCTIONS
+function addTodo(event){
+
+    // PREVENT FORM FROM SUBMITTING
+    event.preventDefault();
+
+    // TODO DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // CREATE LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa fa-check-square"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // CHECK TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv);
+
+    // CLEAR TODO INPUT VALUE
+    todoInput.value = " ";
+ 
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+
+    // DELETE TODO
+    if(item.classList[0] === 'trash-btn') {
+        newTodo.remove();
+    }
+}
